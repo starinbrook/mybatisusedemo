7bef1e9f66b5f3d09292cd93535ff92d13e07d94
zhuyongchang <zhuyongchang@meicai.cn>
2019-08-30 17:06:58 +0800

    增加sql执行方式：1、直接调用SqlSession接口；2、通过SqlSession获取Mapper，然后调用Mapper接口

--- src/main/java/com/zhuyca/kkb/service/UserService.java
+++ src/main/java/com/zhuyca/kkb/service/UserService.java
@@ -1,6 +1,7 @@
 package com.zhuyca.kkb.service;
 
 import com.zhuyca.kkb.entity.User;
+import com.zhuyca.kkb.mapper.UserMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -25,13 +26,22 @@ public class UserService {
         try {
             // 使用mybatis工具类Resources加载全局配置文件（全局配置文件中引用了映射文件，因此也会加载映射文件）
             InputStream inputStream = Resources.getResourceAsStream(resource);
+
             // 使用SqlSessionFactoryBuilder构建SqlSessionFactory
             SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
             // 使用sqlSessionFactory获取SqlSession
             SqlSession sqlSession = sqlSessionFactory.openSession();
-            // 使用SqlSession执行查询sql
+
+            // 执行sql
+            // 方式一：直接调用SqlSession接口
             User user = (User) sqlSession.selectOne("com.zhuyca.kkb.mapper.UserMapper.selectUserById", 1);
+            // 方式二：通过SqlSession获取Mapper类，然后再调用Mapper类的接口
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user1 = userMapper.selectUserById(1);
+
             System.out.println(user.getId() + " " + user.getName() + " " + user.getSex() + " " + user.getAge());
+            System.out.println(user1.getId() + " " + user1.getName() + " " + user1.getSex() + " " + user1.getAge());
         } catch (IOException e) {
             e.printStackTrace();
         }
